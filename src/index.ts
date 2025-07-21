ab3d9cc064bdde0372f4a05398028fd6607fcb30
Eoin Halligan <eoinhalligan3@gmail.com>
2025-07-21 11:35:08 -0400

    Removed environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
 // src/index.ts
 import jwt from 'jsonwebtoken';
 
-const { JWT_SIGNING_KEY } = process.env;
-
-export const verifyAsync = <T>(token: string): Promise<T> => {
+export const verifyAsync = <T>(token: string, signingKey: string): Promise<T> => {
   return new Promise((resolve, reject) => {
-    if (!JWT_SIGNING_KEY) return reject(new Error('No JWT signing key found.'));
-    jwt.verify(token, JWT_SIGNING_KEY, (err, decoded) => {
+    if (!signingKey) return reject(new Error('No JWT signing key provided.'));
+    jwt.verify(token, signingKey, (err, decoded) => {
       if (err) return reject(err);
       return resolve(decoded as T);
     });
   });
 };
 
-export const signAsync = <T extends object>(payload: T): Promise<string> => {
+export const signAsync = <T extends object>(payload: T, signingKey: string): Promise<string> => {
   return new Promise((resolve, reject) => {
-    if (!JWT_SIGNING_KEY) return reject(new Error('No JWT signing key found.'));
-    jwt.sign(payload, JWT_SIGNING_KEY, { algorithm: 'HS256' }, (err, token) => {
+    if (!signingKey) return reject(new Error('No JWT signing key provided.'));
+    jwt.sign(payload, signingKey, { algorithm: 'HS256' }, (err, token) => {
       if (err || !token) return reject(err || new Error('Token creation failed.'));
       return resolve(token);
     });
   });
-};
+
